60433883736bca00b840928a62676c8f7dba51d0
marcosalves <marcosalves276@outlook.com>
2021-11-10 22:03:07 -0300

    encontro presencial 3

--- encontro presencial 3/script.js
+++ encontro presencial 3/script.js
@@ -9,3 +9,25 @@ var div = document.getElementById("usuario")
         div.innerHTML = `Olá ${nome}, seja bem vindo.`
     }
 }
+
+function carrinho (idbutton){
+ //debugger
+ if (typeof listaitens == "undefined"){
+    listaitens = []     
+ }
+    var idtrocado = idbutton.replace("btn","item")
+    var iditem = document.getElementById(idtrocado)
+    var modal = document.getElementById("lista")
+    var desc = iditem.children[1].textContent
+    var preco = iditem.children[2].textContent
+    modal.innerHTML = ""
+    listaitens.push({
+        item : desc,
+        preço : preco
+    })
+
+    for(var x = 0; x < listaitens.length; x++){
+          
+    modal.innerHTML += `<p> ${listaitens[x].item} - ${listaitens[x].preço} </p>`
+    }
+}
